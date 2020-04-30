eebb39a9e99878f9245d45e48c62dff9546e10ca
1072115761@qq.com <1072115761@qq.com>
2020-04-30 18:32:18 +0800

    '添加一个配置文件'

---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="black">
+      <img slot="item-icon" src="../../assets/img/tabbar/psu.jpg" alt="">
+      <img slot="item-icon-active" src="../../assets/img/tabbar/psu_active.jpg" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="blue">
+      <img slot="item-icon" src="../../assets/img/tabbar/psu.jpg" alt="">
+      <img slot="item-icon-active" src="../../assets/img/tabbar/psu_active.jpg" alt="">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="yellow">
+      <img slot="item-icon" src="~assets/img/tabbar/psu.jpg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/psu_active.jpg" alt="">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="blue">
+      <img slot="item-icon" src="../../assets/img/tabbar/psu.jpg" alt="">
+      <img slot="item-icon-active" src="../../assets/img/tabbar/psu_active.jpg" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from 'components/tabbar/TabBar'
+  import TabBarItem from 'components/tabbar/TabBarItem'
+
+  export default {
+    name: "MainTabBar",
+    components:{
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/network/request.js
@@ -0,0 +1,66 @@
+import axios from 'axios'
+
+// export function request(config,success,failure) {
+//   //1.创建axios的实例
+//   const instance = axios.create({
+//     baseURL:'http://123.207.32.32:8000',
+//     timeout:5000
+//   })
+//   //发送网络请求
+//   instance(config)
+//     .then(res =>{
+//       console.log(res);
+//       success(res)
+//     })
+//     .catch(err =>{
+//       console.log(err);
+//       failure(err)
+//     })
+// }
+
+// export function request(config) {
+//   return new Promise((resolve ,reject) =>{
+//
+//     //1.创建axios的实例
+//     const instance = axios.create({
+//       baseURL:'http://123.207.32.32:8000',
+//       timeout:5000
+//     })
+//     //发送网络请求
+//     instance(config)
+//       .then(res =>{
+//       resolve(res)
+//       })
+//       .catch(err =>{
+//         reject(err)
+//       })
+//   })
+// }
+export function request(config) {
+
+    //1.创建axios的实例
+    const instance = axios.create({
+      baseURL:'http://123.207.32.32:8000',
+      timeout:5000
+    })
+  //2.axios拦截器
+  instance.interceptors.request.use(config => {
+    console.log(config);
+    return config
+  }, err => {
+    console.log(err);
+  });
+    //响应拦截
+
+    instance.interceptors.response.use(res => {
+      console.log(res);
+    },err => {
+      console.log(err);
+    })
+    //发送网络请求
+    return instance(config)
+
+
+}
+
+
